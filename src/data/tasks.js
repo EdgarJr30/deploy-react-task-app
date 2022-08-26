b234f7ad32a8af2d7b190cafbead0f349434c584
Edgar Perez <edgarjoel9912@gmail.com>
2022-08-26 17:35:58 -0400

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+    {
+        id: 0,
+        title: 'Primera tárea',
+        description: 'Tárea 1'
+    },
+    {
+        id: 1,
+        title: 'Segunda tárea',
+        description: 'Tárea 2'
+    },
+    {
+        id: 2,
+        title: 'Tercera tárea',
+        description: 'Tárea 3'
+    }
+]
